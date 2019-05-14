7e933c12918c01f20ad474e3744e743210157f1f
Brett Johnson <br.onson@outlook.com>
2019-05-14 13:22:14 -0500

    init

---
+++ CustomList/CustomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        //member variables (HAS A)
+        T[] items = new T[4];
+
+        //constructor (SPAWNER)
+
+        //member methods (CAN DO)
+    }
+}
---
+++ UnitTestProject1/AddUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class AddUnitTests
+    {
+        [TestMethod]
+        public void AddPositiveInt_TestMethod1()
+        {
+            //arrange
+
+            //act
+            
+
+            //assert
+        }
+    }
+}
