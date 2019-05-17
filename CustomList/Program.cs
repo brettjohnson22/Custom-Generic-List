06a3c7e8fa76217d272b80910ce96bf3440d4762
Brett Johnson <br.onson@outlook.com>
2019-05-17 13:51:45 -0500

    removed test code in program

--- CustomList/Program.cs
+++ CustomList/Program.cs
@@ -10,17 +10,7 @@ namespace CustomList
     {
         static void Main(string[] args)
         {
-            CustomList<int> test = new CustomList<int>();
-            test.Add(2);
-            test.Add(4);
-            test.Add(1);
-            test.Add(6);
-            test.Add(8);
 
-            Console.WriteLine(test.Count);
-            Console.ReadLine();
-
-            List<int> tesert = new List<int>();
         }
     }
 }
